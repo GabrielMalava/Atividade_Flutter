64a50f64d3bf334f6f7ea9549798bc97d103faf5
Gabriel Malavazi Rodrigues <145515242+GabrielMalava@users.noreply.github.com>
2024-11-11 19:48:19 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart'; // Certifique-se de que o caminho está correto
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(), // Define HomeScreen como a tela inicial
+      home: HomeScreen(), 
+      
     );
   }
 }
